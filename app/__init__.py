a4e4116a755b4353cc9e794ed8f3f3758a6df234
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-10-28 22:19:36 -0400

    make blogs

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import sqlite3
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def login():
-    return render_template("home.html", logged=False, blogs=[])
+    return render_template("home.html", logged=False, blogs=[], user="", id=0, passw="")
 
 @app.route("/<id>/<username>/<password>", methods=["GET"])
 def index(id, username, password):
@@ -14,7 +14,7 @@ def index(id, username, password):
         if filename.is_file():
             file = open(filename.path)
             files.append(file.read())
-    return render_template("home.html", logged=True, blogs=files)
+    return render_template("home.html", logged=True, blogs=files, user=username, id=id, passw=password)
     
 @app.route("/auth", methods=["GET", "POST"])
 def auth():
@@ -23,12 +23,31 @@ def auth():
     c.execute("SELECT * FROM USER WHERE username=? AND password=?", (request.form['username'], request.form['password']))
     user = c.fetchone()
     if user != None:
+        db.close()
         return redirect("/{id}/{username}/{password}".format(id=user[0], username=user[1], password=user[2]))
+    db.close()
     return render_template("error.html", msg=1)
 
 @app.route("/logout")
 def logout():
     return redirect("/")
+
+@app.route("/new_entry/<id>/<username>/<password>", methods=["POST", "GET"])
+def new_entry(id, username, password):
+    if request.method == "POST":
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        c.execute("SELECT last_post_number FROM user WHERE id = ?", (id,))
+        file = open("./blogs/{id}/{text}.txt".format(id=id, text=(c.fetchone()[0] + 1)), "w")
+        c.execute("UPDATE user SET last_post_number=last_post_number+1 WHERE id=?", (id,))
+        file.write(request.form['new_entry'])
+        file.close()
+        db.commit()
+        db.close()
+        return redirect("/{id}/{username}/{password}".format(id=id, username=username, password=password))
+    else:
+        return render_template("new_entry.html", id=id, user=username, passw=password)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def make():
