96a132c2c844b520b7a65243bb0d9ad4b1c45714
sookiemonster <sookiemonster216@gmail.com>
2021-10-31 21:11:22 -0400

    Update home(), auth() & my_blog() to work with session variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,56 @@
 from flask import Flask, render_template, request, session, redirect
-import os
-import sqlite3
+import os, sqlite3
+
+# DEFINE CONSTANTS
+USER_TABLE = "USERS"
+DB_FILE = "users.db"
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
+
 @app.route("/", methods=["GET"])
-def login():
-    if 'username' in session.keys() and 'password' in session.keys():
+def home():
+    if (session['username']):
+        return render_template("home.html") # Render home if user is logged in
+    return render_template("login.html") # Render login page if not logged in
+
+@app.route("/my_blog", methods=["GET"])
+def user_blog():
+    # Retrieve & render user's blog if they are logged in
+    if (session['username']): 
         files = []
-        db = sqlite3.connect("users.db")
+        db = sqlite3.connect(DB_FILE)
         c = db.cursor()
-        c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID = ?", (session['UID'],))
-        for filename in range(c.fetchone()[0], -1, -1):
-            file = open("./blogs/{id}/{filename}.txt".format(id=session['UID'], filename=filename))
-            files.append(file.read())
-        return render_template("home.html", logged=True, blogs=files, user=session['username'], id=session['UID'], passw=session['password'])
+
+        get_UID = "SELECT UID FROM %s WHERE USERNAME = %s" % (USER_TABLE, session['username'])
+        c.execute(get_UID)
+        id = c.fetchone()
+
+        get_last_post_num = "SELECT LAST_POST_NUM FROM %s WHERE id = %s" % (USER_TABLE, id)
+        c.execute(get_last_post_num)
+        last_post_num = c.fetchone()
+
+        for post_num in range(last_post_num, -1, -1): # Descend from the last post to 0
+            post = open("./blogs/%s/%s.txt".format(id, post_num))
+            files.append(post.read())
+
+        return render_template("home.html", blogs=files)
     else:
-        db = sqlite3.connect("users.db")
-        c = db.cursor()
-        make_user_table = ("""CREATE TABLE IF NOT EXISTS USERS(
-                UID INTEGER PRIMARY KEY NOT NULL,
-                USERNAME TEXT NOT NULL,
-                PASSWORD TEXT NOT NULL,
-                BLOG_NAME TEXT,
-                LAST_POST_NUM INTEGER, 
-                UNIQUE (USERNAME));""")
-                
-        c.execute(make_user_table)
-        db.commit()
-        db.close()
-        return render_template("login.html", logged=False, blogs=[], user="", id=0, passw="")
+        return redirect("/")
     
 @app.route("/auth", methods=["GET", "POST"])
 def auth():
-    db = sqlite3.connect("users.db")
+    db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute("SELECT * FROM USERS WHERE USERNAME=? AND PASSWORD=?", (request.form['username'], request.form['password']))
+
+    client_username = request.form['username']
+    client_password = request.form['password']
+
+    c.execute("SELECT * FROM USER WHERE username=? AND password=?", (client_username, client_password))
     user = c.fetchone()
+
     if user != None:
-        session['UID'] = user[0]
         session['username'] = user[1]
-        session['password'] = user[2]
         db.close()
         return redirect("/")
     db.close()
@@ -71,16 +81,32 @@ def new_entry():
 @app.route("/register", methods=["GET", "POST"])
 def make():
     if request.method == "POST":
+        client_username = request.form['username']
+        client_password = request.form['password']
+
         db = sqlite3.connect("users.db")
         c = db.cursor()
+
+        make_user_table = ("""CREATE TABLE IF NOT EXISTS USERS(
+            UID INTEGER PRIMARY KEY NOT NULL,
+            USERNAME TEXT NOT NULL,
+            PASSWORD TEXT NOT NULL,
+            BLOG_NAME TEXT
+            LAST_POST_NUM INTEGER, 
+            UNIQUE (USERNAME));""")
+            
+        c.execute(make_user_table)
+
+        # list_users = 
+
         try:
             # Add user credentials to database
-            c.execute("INSERT INTO USERS(USERNAME, PASSWORD, LAST_POST_NUM) VALUES(?, ?, ?)", 
-                (request.form['username'], request.form['password'], -1))
+            c.execute("INSERT INTO USERS(USERNAME, PASSWORD) VALUES(?, ?)", 
+                (client_username, client_password))
             
             # Select newly created user
-            c.execute("SELECT * FROM USERS WHERE USERNAME=? AND PASSWORD=?", 
-                (request.form['username'], request.form['password']))
+            c.execute("SELECT * FROM USER WHERE USERNAME=? AND PASSWORD=?", 
+                (client_username, client_password))
 
             new_user = c.fetchone()
             os.mkdir("./blogs/%s" % (new_user[0])) # Create a blog directory to store new user's files
