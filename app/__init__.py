620ede94c03bd603cf8d318cf0de1ca3b041aa6e
Joshua-Kloepfer20 <jkloepfer20@stuy.edu>
2021-11-01 19:19:40 -0400

    work :)

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = os.urandom(32)
 @app.route("/", methods=["GET"])
 def login():
-    if 'username' in session.keys() and 'password' in session.keys():
+    if 'username' in session.keys():
         files = []
         db = sqlite3.connect("users.db")
         c = db.cursor()
@@ -14,7 +14,7 @@ def login():
         for filename in range(c.fetchone()[0], -1, -1):
             file = open("./blogs/{id}/{filename}.txt".format(id=session['UID'], filename=filename))
             files.append(file.read())
-        return render_template("home.html", logged=True, blogs=files, user=session['username'], id=session['UID'], passw=session['password'])
+        return render_template("home.html", logged=True, blogs=files)
     else:
         db = sqlite3.connect("users.db")
         c = db.cursor()
@@ -33,18 +33,20 @@ def login():
 
 @app.route("/auth", methods=["GET", "POST"])
 def auth():
-    db = sqlite3.connect("users.db")
-    c = db.cursor()
-    c.execute("SELECT * FROM USERS WHERE USERNAME=? AND PASSWORD=?", (request.form['username'], request.form['password']))
-    user = c.fetchone()
-    if user != None:
-        session['UID'] = user[0]
-        session['username'] = user[1]
-        session['password'] = user[2]
+    if (request.method == "POST"):
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        c.execute("SELECT * FROM USERS WHERE USERNAME=? AND PASSWORD=?", (request.form['username'], request.form['password']))
+        user = c.fetchone()
+        if user != None:
+            session['UID'] = user[0]
+            session['username'] = user[1]
+            db.close()
+            return redirect("/")
         db.close()
+        return render_template("error.html", msg=2)
+    else:
         return redirect("/")
-    db.close()
-    return render_template("error.html", msg=1)
 
 @app.route("/logout")
 def logout():
@@ -53,19 +55,22 @@ def logout():
 
 @app.route("/new_entry", methods=["POST", "GET"])
 def new_entry():
-    if request.method == "POST":
-        db = sqlite3.connect("users.db")
-        c = db.cursor()
-        c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID = ?", (session['UID'],))
-        file = open("./blogs/{id}/{text}.txt".format(id=session['UID'], text=(c.fetchone()[0] + 1)), "w")
-        c.execute("UPDATE USERS SET LAST_POST_NUM=LAST_POST_NUM+1 WHERE UID=?", (session['UID'],))
-        file.write(request.form['new_entry'])
-        file.close()
-        db.commit()
-        db.close()
-        return redirect("/")
+    if 'username' in session.keys():
+        if request.method == "POST":
+            db = sqlite3.connect("users.db")
+            c = db.cursor()
+            c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID = ?", (session['UID'],))
+            file = open("./blogs/{id}/{text}.txt".format(id=session['UID'], text=(c.fetchone()[0] + 1)), "w")
+            c.execute("UPDATE USERS SET LAST_POST_NUM=LAST_POST_NUM+1 WHERE UID=?", (session['UID'],))
+            file.write(request.form['new_entry'])
+            file.close()
+            db.commit()
+            db.close()
+            return redirect("/")
+        else:
+            return render_template("new_entry.html")
     else:
-        return render_template("new_entry.html")
+        return redirect("/")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -90,36 +95,50 @@ def make():
         db.close()
         return redirect("/")
     else:
-        return render_template("register.html", taken=False)
+        return render_template("error.html", msg=1)
 @app.route("/edit/<int:index>", methods=["GET", "POST"])
 def edit(index):
-    db = sqlite3.connect("users.db")
-    c = db.cursor()
-    c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID=?", (session['UID'],))
-    user = c.fetchone()
-    if request.method == "POST":
-        file = open("./blogs/{id}/{index}.txt".format(id = session['UID'], index=user[0] - index), "w")
-        file.write(request.form['edit'])
-        return redirect('/')
+    if 'username' in session.keys():
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID=?", (session['UID'],))
+        user = c.fetchone()
+        if request.method == "POST":
+            file = open("./blogs/{id}/{index}.txt".format(id = session['UID'], index=user[0] - index), "w")
+            file.write(request.form['edit'])
+            return redirect('/')
+        else:
+            file = open("./blogs/{id}/{index}.txt".format(id = session['UID'], index=user[0] - index), "r")
+            return render_template("edit.html", text=file.read(), index=index)
     else:
-        file = open("./blogs/{id}/{index}.txt".format(id = session['UID'], index=user[0] - index), "r")
-        return render_template("edit.html", text=file.read(), index=index)
+        return redirect("/")
 @app.route("/view")
 def view():
-    directories = []
-    for dir in os.scandir("./blogs/"):
-        directories.append(dir)
-    return render_template("view.html", items=directories)
-@app.route("/view/<int:UID>")
-def viewid(UID):
-    files = []
-    db = sqlite3.connect("users.db")
-    c = db.cursor()
-    c.execute("SELECT LAST_POST_NUM FROM USERS WHERE UID = ?", (session['UID'],))
-    for filename in range(c.fetchone()[0], -1, -1):
-        file = open("./blogs/{id}/{filename}.txt".format(id=session['UID'], filename=filename))
-        files.append(file.read())
-    return render_template("look.html", items=files)
+    if 'username' in session.keys():
+        directories = []
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        for dir in os.scandir("./blogs"):
+            if (int)(dir.name) != session['UID']:
+                c.execute("SELECT USERNAME FROM USERS WHERE UID=?", (dir.name,))
+                directories.append(c.fetchone()[0])
+        return render_template("view.html", blogs=directories)
+    else:
+        return redirect("/")
+@app.route("/view/<username>")
+def viewid(username):
+    if 'username' in session.keys():
+        files = []
+        db = sqlite3.connect("users.db")
+        c = db.cursor()
+        c.execute("SELECT LAST_POST_NUM, UID FROM USERS WHERE USERNAME = ?", (username,))
+        user = c.fetchone()
+        for filename in range(user[0], -1, -1):
+            file = open("./blogs/{id}/{filename}.txt".format(id=user[1], filename=filename))
+            files.append(file.read())
+        return render_template("look.html", blogs=files)
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
     app.debug = True
